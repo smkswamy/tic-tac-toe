6678380405058dd4a9f9e540217fccc493064738
Manikantha <smkswamy123@gmail.com>
2025-01-03 16:39:21 -0500

    Adding tic tac toe game changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
 import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import Game from "./api/_game";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function App() {
+  return (
+    <div>
+      <Game />
+    </div>
+  )
 }
---
+++ src/pages/api/_square.tsx
@@ -0,0 +1,14 @@
+interface SquareProps {
+    value: string;
+    onSquareClick: () => void;
+}
+
+function Square({ value, onSquareClick }: SquareProps) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+
+export default Square;
